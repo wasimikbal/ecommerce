5d754ee5e6b8c6b873a73cbc1678ba75f4bcd56d
wasimikbal <44136359+wasimikbal@users.noreply.github.com>
2024-06-09 21:00:22 +0500

    Product details and cart

--- app/components/ProductDetails.tsx
+++ app/components/ProductDetails.tsx
@@ -4,6 +4,7 @@ import { Product as ProductType } from '@/types/product'
 import { AiFillStar, AiOutlineMinus, AiOutlinePlus, AiOutlineStar } from "react-icons/ai";
 import Product from "@/app/components/Product";
 import { urlFor } from '@/lib/client';
+import { useProductsContext } from '@/context/StateContext';
 
 interface ProductDetailsProps {
     product: ProductType;
@@ -12,72 +13,70 @@ interface ProductDetailsProps {
 
 const ProductDetails: React.FC<ProductDetailsProps> = ({product, productList}) => {
     const {name, details, price, image} = product;
-    const [_product, setProduct] = useState([]);
-    
-
+    const [index, setIndex] = useState<number>(0);
+    const {cartItems, qty, inqQty, decQty, onAddToCart} = useProductsContext();
+ 
   return (
     
     <div>
       <div className="product-detail-container">
         <div>
           <div className="image-container">
-            <img src={urlFor(product.image && product.image[0]).toString()}></img>
+            <img src={urlFor(image && image[index]).toString()} className="product-detail-image" />
           </div>
           <div className="small-images-container">
-            {
-              image.map((img, i)=>(
-                <img src={urlFor(img).toString()}
-                className=""
-                onMouseEnter={()=>{}
-              }></img>
-              ))
-            }
+            {image?.map((item, i) => (
+              <img 
+                key={i}
+                src={urlFor(item).toString()}
+                className={i === index ? 'small-image selected-image' : 'small-image'}
+                onMouseEnter={() => setIndex(i)}
+              />
+            ))}
           </div>
         </div>
+
         <div className="product-detail-desc">
           <h1>{name}</h1>
           <div className="reviews">
-            <AiFillStar/>
-            <AiFillStar/>
-            <AiFillStar/>
-            <AiFillStar/>
-            <AiOutlineStar/>
-          <p>(20)</p>
+            <div>
+              <AiFillStar />
+              <AiFillStar />
+              <AiFillStar />
+              <AiFillStar />
+              <AiOutlineStar />
+            </div>
+            <p>
+              (20)
+            </p>
           </div>
-          <h4>Details:</h4>
+          <h4>Details: </h4>
           <p>{details}</p>
           <p className="price">${price}</p>
           <div className="quantity">
             <h3>Quantity:</h3>
             <p className="quantity-desc">
-              <span onClick={()=>{}} className="minus">
-                <AiOutlineMinus/>
-              </span>
-              <span className="num">
-                0
-              </span>
-              <span className="plus">
-                <AiOutlinePlus/>
-              </span>
+              <span className="minus" onClick={decQty}><AiOutlineMinus /></span>
+              <span className="num">{qty}</span>
+              <span className="plus" onClick={inqQty}><AiOutlinePlus /></span>
             </p>
           </div>
           <div className="buttons">
-            <button className="add-to-cart">Add To Cart</button>
-            <button className="buy-now">Buy Now</button>
+            <button type="button" className="add-to-cart" onClick={() => {onAddToCart(product, qty)}}>Add to Cart</button>
+            <button type="button" className="buy-now" onClick={()=>{}}>Buy Now</button>
           </div>
         </div>
       </div>
+
       <div className="maylike-products-wrapper">
-        <h2>You may also like</h2>
-        <div className="marquee">
-          <div className="maylike-products-container track">
-            {
-              productList.map((item: ProductType)=>(
-                <Product key={item._id} {...item}/>
-              ))
-            }
+          <h2>You may also like</h2>
+          <div className="marquee">
+            <div className="maylike-products-container track">
+              {productList.map((item) => (
+                <Product key={item._id} {...item} />
+              ))}
+            </div>
           </div>
-        </div>
       </div>
     </div>
   )
