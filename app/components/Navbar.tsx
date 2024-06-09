dcb2c7e6f955294b10c6eb32716f939b39629b86
wasimikbal <44136359+wasimikbal@users.noreply.github.com>
2024-06-09 20:59:54 +0500

    state management

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,17 +2,25 @@
 import React from 'react'
 import Link from 'next/link'
 import { AiOutlineShopping } from 'react-icons/ai'
+import Cart from './Cart'
+import { useProductsContext } from '@/context/StateContext'
 
 const Navbar = () => {
+  const {showCart, setShowCart, totalQuantities} = useProductsContext();
+
+
   return (
     <div className='navbar-container'>
       <p className="logo">
         <Link href={`/`}>Ecommerce Shop</Link>
       </p>
-      <button className='cart-icon' type='button' onClick={()=>{}}>
+      <button className='cart-icon' type='button' onClick={()=>{
+        setShowCart((prev)=> !prev)
+      }}>
         <AiOutlineShopping/>
-        <span className='cart-item-qty'>1</span>
+        <span className='cart-item-qty'>{totalQuantities}</span>
       </button>
+      {showCart && <Cart/>}
     </div>
   )
 }
